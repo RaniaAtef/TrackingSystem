16e0d1efb4ed4b5b7a2c30b1f68c5f949fb1c3eb
Rania Atef <raniaatef718@gmail.com>
2024-02-17 01:45:43 +0200

    change image path

--- src/components/ShipmentDelivery.jsx
+++ src/components/ShipmentDelivery.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import img from "../../public/problem.png";
 const ShipmentDelivery = () => {
   const { t, i18n } = useTranslation();
 
@@ -8,7 +7,7 @@ const ShipmentDelivery = () => {
     <div className="ml-8">
       <div className="card card-side bg-base-100 shadow-xl w-96 mt-5">
         <figure className="w-96 p-5">
-          <img src={img} />
+          <img src="./problem.png" alt="Movie" />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
