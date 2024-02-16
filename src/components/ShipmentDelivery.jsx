40416c835ae8959fc37e2662a62c6ed1a9fcc6bf
Rania Atef <raniaatef718@gmail.com>
2024-02-17 01:41:23 +0200

    images import

--- src/components/ShipmentDelivery.jsx
+++ src/components/ShipmentDelivery.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-
+import img from "../../public/problem.png";
 const ShipmentDelivery = () => {
   const { t, i18n } = useTranslation();
 
@@ -8,7 +8,7 @@ const ShipmentDelivery = () => {
     <div className="ml-8">
       <div className="card card-side bg-base-100 shadow-xl w-96 mt-5">
         <figure className="w-96 p-5">
-          <img src="../../public/problem.png" alt="Movie" />
+          <img src={img} />
         </figure>
         <div className="card-body">
           <h2 className="card-title">
